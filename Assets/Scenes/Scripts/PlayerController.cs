4bc2ce0b236f0c27a3257eb58c084714ce102145
duongnguyen27903 <duongnguyen27903@gmail.com>
2023-09-10 18:29:18 +0000

    feat : create player and laze, player can shoot laze k.o enemy

---
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section3
+{
+    public class PlayerController : MonoBehaviour
+    {
+        [SerializeField] private float MoveSpeed;
+        [SerializeField] private ProjectTileController ProjectTile;
+        [SerializeField] private Transform FiringPoint;
+        [SerializeField] private float FiringCooldown;
+
+        private float TempCooldown;
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            float horizontal = Input.GetAxis("Horizontal");
+            float vertical = Input.GetAxis("Vertical");
+            Vector2 direction = new Vector2(horizontal, vertical);
+            transform.Translate(direction * Time.deltaTime * MoveSpeed);
+
+            if( Input.GetKey(KeyCode.Space) )
+            {
+                if( TempCooldown <=0 )
+                {
+                    Fire();
+                    TempCooldown = FiringCooldown;
+                }
+                TempCooldown -= Time.deltaTime;
+            }
+        }
+
+        private void Fire()
+        {
+            ProjectTileController projectTile =  Instantiate(ProjectTile, FiringPoint.position,Quaternion.identity,null);
+            projectTile.Fire();
+        }
+    }
+
+}
---
+++ Assets/Scenes/Scripts/ProjectTileController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Section3
+{
+    public class ProjectTileController : MonoBehaviour
+    {
+        [SerializeField] private float MoveSpeed;
+        [SerializeField] private Vector2 Direction; 
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.Translate(Direction * Time.deltaTime * MoveSpeed);
+        }
+
+        public void Fire()
+        {
+            Destroy(gameObject, 3f);
+        }
+    }
+
+}
